d544556766cab194d628c92b3895b170e945c2f1
manojkalyan27 <147662541+manojkalyan27@users.noreply.github.com>
2024-12-03 18:07:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const check = document.querySelector('#check');
+const submitButton = document.querySelector('#submit');
+
+check.addEventListener('change', ()=>{
+    submitButton.disabled = !check.checked;
+});
